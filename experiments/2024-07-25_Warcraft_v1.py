881922db44df260a125e03ed0e3f3444c735fa47
k-onoue <k.onoue0724@gmail.com>
2024-07-25 11:08:22 +0900

    warcraft objective needs to be reconsidered in terms of the evaluation of the end point

--- experiments/2024-07-25_Warcraft_v1.py
+++ experiments/2024-07-25_Warcraft_v1.py
@@ -3,7 +3,6 @@ import os
 import json
 import numpy as np
 import optuna
-from multiprocessing import Process
 
 from path_info import PROJECT_DIR, EXPT_RESULT_DIR
 sys.path.append(PROJECT_DIR)
@@ -11,18 +10,15 @@ from src.objectives_optuna import WarcraftObjective
 from src.utils_experiment import extract_info_from_filename
 
 
-
 def objective_wrapper(weight_matrix):
     objective = WarcraftObjective(weight_matrix)
     return objective
 
-def run_study(storage, weight_matrix, sampler):
+
+def run_study(weight_matrix, sampler, results_dir):
     study = optuna.create_study(
         direction='minimize',
-        sampler=sampler,
-        storage=storage,
-        study_name='exhaustive_seach',
-        load_if_exists=True
+        sampler=sampler
     )
     objective = objective_wrapper(weight_matrix)
     study.optimize(objective)
@@ -36,11 +32,9 @@ def run_study(storage, weight_matrix, sampler):
         'number': best_trial.number,
     }
     # JSONファイルに保存
-    with open(os.path.join(EXPT_RESULT_DIR, 'exhaustive_seach.json'), 'w') as f:
+    with open(os.path.join(results_dir, 'exhaustive_search.json'), 'w') as f:
         json.dump(best_trial_data, f, indent=4)
-    print(f"Best trial saved in {os.path.join(EXPT_RESULT_DIR, 'exhaustive_seach.json')}")
-
-
+    print(f"Best trial saved in {os.path.join(results_dir, 'exhaustive_search.json')}")
 
 
 if __name__ == '__main__':
@@ -53,28 +47,14 @@ if __name__ == '__main__':
     results_dir = os.path.join(EXPT_RESULT_DIR, date, objective_function)
     os.makedirs(results_dir, exist_ok=True)
 
-
     optuna.logging.set_verbosity(optuna.logging.INFO)
 
     weight_matrix = np.array([
-        [0.1, 0.4, 0.9],
-        [0.4, 0.1, 0.4],
-        [0.9, 0.4, 0.1]
+        [0.1, 0.9],
+        [0.4, 0.1],
     ])
-
 
     sampler = optuna.samplers.BruteForceSampler()
 
-    storage = 'sqlite:///exhaustive_seach.db'  # SQLiteデータベースを使用
-
-    # プロセスの数を指定
-    n_processes = os.cpu_count()
-
-    processes = []
-    for _ in range(n_processes):
-        p = Process(target=run_study, args=(storage, weight_matrix, sampler))
-        p.start()
-        processes.append(p)
-
-    for p in processes:
-        p.join()
+    # 最適化を実行
+    run_study(weight_matrix, sampler, results_dir)
