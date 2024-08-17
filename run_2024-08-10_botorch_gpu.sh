fe99c8fd50574450315a23a3723a81fb1245d849
k-onoue <k.onoue0724@gmail.com>
2024-08-17 10:56:24 +0900

    set up an experiment

--- run_2024-08-10_botorch_gpu.sh
+++ run_2024-08-10_botorch_gpu.sh
@@ -2,11 +2,11 @@
 
 # Resource configuration
 CPUS_PER_TASK=3  # Adjust the number of CPUs per task
-PARTITION="gpu_short"  # Partition name
-TIME="03:00:00"  # Maximum execution time
+PARTITION="gpu_long"  # Partition name
+TIME="09:30:00"  # Maximum execution time
 
 # Create results directory if it doesn't exist
-mkdir -p results/$RES_DIR_NAME
+mkdir -p results/
 
 # Create logs directory if it doesn't exist
 mkdir -p logs
@@ -30,8 +30,15 @@ cat $config_file
 # Run multiple Python files in parallel using sbatch
 sbatch --job-name=benchmark_${PARTITION} \
        --output=results/output_constraint_%j.txt \
-       --gres=gpu:1 \
        --cpus-per-task=$CPUS_PER_TASK \
        --partition=$PARTITION \
        --time=$TIME \
-       --wrap="python3 experiments/2024-08-11_botorch/Griewank_3d_constrained1.py"
+       --wrap="python3 experiments/2024-08-17_botorch/Griewank_3d_constrained1.py"
+
+# Run multiple Python files in parallel using sbatch
+sbatch --job-name=benchmark_${PARTITION} \
+       --output=results/output_constraint_%j.txt \
+       --cpus-per-task=$CPUS_PER_TASK \
+       --partition=$PARTITION \
+       --time=$TIME \
+       --wrap="python3 experiments/2024-08-17_botorch/Griewank_3d_constrained1.py"
