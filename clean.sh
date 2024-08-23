6758bb3d0ff645f3746ed28829e1fc3fb5dcadb6
k-onoue <k.onoue0724@gmail.com>
2024-08-23 21:58:11 +0900

    modify

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 現在の時間を「何時何分」の形式で取得
-CURRENT_TIME=$(date +"%H-%M")
+# 現在の時間を「年-月-日_時-分」の形式で取得
+CURRENT_TIME=$(date +"%Y-%m-%d_%H-%M")
 
 # アーカイブディレクトリのパスを設定
 ARCHIVE_DIR="experiments/archive"
@@ -12,10 +12,10 @@ mkdir -p $ARCHIVE_DIR
 # ルートディレクトリのrun_*.shファイルをアーカイブディレクトリに移動
 for file in run_*.sh; do
     if [ -f "$file" ]; then
-        # ファイル名に現在の時間を追加して移動
+        # ファイル名に現在の日時を追加して移動
         mv "$file" "$ARCHIVE_DIR/${file%.sh}_$CURRENT_TIME.sh"
     fi
 done
 
 # 処理完了メッセージ
-echo "All run_*.sh files have been moved to $ARCHIVE_DIR/ with the time (HH-MM) appended."
+echo "All run_*.sh files have been moved to $ARCHIVE_DIR/ with the date and time (YYYY-MM-DD_HH-MM) appended."
